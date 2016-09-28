bf751e72f8e468679f95f294b98ca866e6d8d368
Vlad <vladislav.mokriy@isells.eu>
2016-09-28 22:03:38 +0300

    FIX home task unit 2 - add functional component ProductColorItem - send object and array with multer

--- client/modules/Product/pages/ProductFormPage/ProductFormPage.js
+++ client/modules/Product/pages/ProductFormPage/ProductFormPage.js
@@ -12,18 +12,6 @@ import  styles from './ProductFormPage.css'
 
 const sizes = ['XS', 'S', 'M', 'L', 'XL'];
 const colors = ['white', 'red', 'blue', 'green', 'black'];
-const serialize = (obj, prefix) => {
-  let str = [];
-  for(let p in obj) {
-    if (obj.hasOwnProperty(p)) {
-      var k = prefix ? prefix + "[" + p + "]" : p, v = obj[p];
-      str.push(typeof v == "object" ?
-        serialize(v, k) :
-      encodeURIComponent(k) + "=" + encodeURIComponent(v));
-    }
-  }
-  return str.join("&");
-};
 
 class ProductFormPage extends Component {
   constructor(props) {
